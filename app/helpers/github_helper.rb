ac378c456bf195c17e753f98b0e1e2931e630c6e
rhannequin <remy.hannequin@gmail.com>
2013-06-03 18:15:12 +0200

    Fix user links

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -3,7 +3,9 @@ module GithubHelper
     "http://github.com/#{username}"
   end
 
-  def repo_link(username, repo)
-    "http://github.com/#{username}/#{repo}"
+  def repo_link(username, repo, add_at_sign = false)
+    link = "http://github.com/"
+    link += "@" if add_at_sign
+    link += "#{username}/#{repo}"
   end
 end
